4164515bdfd2c5443d7998787ff241523319b9b8
geddie212 <pauliusgedrimas@gmail.com>
2022-06-13 16:12:25 +0100

    add various files

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+SQL = os.environ.get('DATABASE_URL').split('postgres')
+SQL[0] += 'postgresql'
+updated_SQL = f'{SQL[0]}{SQL[1]}'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = updated_SQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# SECRET KEY SETUP
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
+
 
 
 class Task(db.Model):
@@ -12,11 +22,38 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(500), unique=True, nullable=False)
     completed = db.Column(db.Boolean(), nullable=False)
+    editable = db.Column(db.Boolean(), nullable=False)
+
+
+db.create_all()
+db.session.commit()
 
 
 @app.route('/finish_task/<int:task_id>')
 def finish_task(task_id):
-    Task.query.filter_by(id=task_id).update({'completed': True})
+    task = Task.query.filter_by(id=task_id).first()
+    if task.completed:
+        task.completed = False
+    elif not task.completed:
+        task.completed = True
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/convert_task/<int:task_id>')
+def convert_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    task.editable = True
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/edit_task/<int:task_id>', methods=['POST'])
+def edit_task(task_id):
+    content = request.form['task']
+    task = Task.query.filter_by(id=task_id).first()
+    task.task = content
+    task.editable = False
     db.session.commit()
     return redirect(url_for('home'))
 
@@ -33,7 +70,7 @@ def add_task():
     task = request.form['task']
     query = Task.query.filter(Task.task == task).all()
     if len(query) == 0:
-        db.session.add(Task(task=task, completed=False))
+        db.session.add(Task(task=task, completed=False, editable=False))
         db.session.commit()
     return redirect(url_for('home'))
 
